b676553eff680b5cadcaac80e136ca4a7ce200d4
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-05-30 21:25:42 +0800

    Modified global constants, including a new URL constructor

--- Eventure/Global Constants.swift
+++ Eventure/Global Constants.swift
@@ -9,8 +9,8 @@
 
 import UIKit
 
-/// The URL prefix for all the APIs
-let API_BASE_URL = "https://api.eventure-app.com"
+/// The URL prefix for all the APIs.
+let API_BASE_URL = "https://api.eventure-app.com/"
 
 /// Credentials: DO NOT include when committing
 let USERNAME = "__replace__"
@@ -22,7 +22,7 @@ let MAIN_TINT = UIColor(red: 0.5, green: 0.7, blue: 0.92, alpha: 1)
 /// Todo: REPLACE THIS WITH THE NAVIGATION BAR COLOR
 let NAVBAR_TINT = UIColor(white: 0.93, alpha: 1)
 
-/// Custom URLSessionConfiguration with no caching
+/// Custom URLSessionConfiguration with no caching.
 let CUSTOM_SESSION: URLSession = {
     let config = URLSessionConfiguration.default
     config.requestCachePolicy = .reloadIgnoringLocalCacheData
@@ -31,16 +31,48 @@ let CUSTOM_SESSION: URLSession = {
     return URLSession(configuration: config)
 }()
 
-/// URL Encode & Decode
 extension String {
+    
+    /// URL encode.
     var encoded: String {
-        return self.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? self
+        return self.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? self
     }
     
+    /// URL decode.
     var decoded: String {
         return self.removingPercentEncoding ?? self
     }
 }
 
-/// Documents directory URL
+/// Initialize new URL with url parameters
+
+extension URL {
+    
+    /**
+     Custom URL formatter.
+     
+     - Parameters:
+     - base: The base / parent URL of the API.
+     - API_Name: The API name.
+     - parameters: The url parameters in the form of a Swift native dictionary.
+     
+     - Returns: An optional URL object (URL?) formed from the provided information.
+     */
+    
+    static func with(base: String, API_Name: String, parameters: [String: String]) -> URL? {
+        if (parameters.isEmpty) {
+            return URL(string: base + API_Name)
+        }
+        let formattedParameters = parameters.map {
+            return $0.key.encoded + "=" + $0.value.encoded
+        }
+        return URL(string: "\(base + API_Name)?" + formattedParameters.joined(separator: "&"))
+    }
+}
+
+
+/// Documents directory URL.
 let DOCUMENTS_URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+/// Cache directory URL.
+let CACHES = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
