b6dbb3567a8fe29aa59030e8b3b9e31603ae8ff5
Victor_Li <lytz@berkeley.edu>
2019-06-01 14:44:23 -0700

    resolved codes between Shanrui and I, added in response based on server message, register page(waiting for API)

--- Eventure/Global Constants.swift
+++ Eventure/Global Constants.swift
@@ -6,17 +6,20 @@
 //  Copyright © 2019 UC Berkeley. All rights reserved.
 //
 //  This file documents all global constants used by the app
-
 import UIKit
 
 /// The URL prefix for all the APIs
 let API_BASE_URL = "https://api.eventure-app.com/"
 
-/// Todo: REPLACE THIS WITH THE APP's THEME COLOR
+/// Credentials: DO NOT include when committing
+let USERNAME = "__replace__"
+let PASSWORD = "__replace__"
 
+/// Todo: REPLACE THIS WITH THE APP's THEME COLOR
 let MAIN_TINT2 = UIColor(red: 0.5, green: 0.7, blue: 0.92, alpha: 1)
 let MAIN_TINT = UIColor(red: 127/255, green: 219/255, blue: 203/255, alpha: 1.0)
 let MAIN_TINT_DARK = UIColor(red: 73/255, green: 188/255, blue: 167/255, alpha: 1)
+
 /// Todo: REPLACE THIS WITH THE NAVIGATION BAR COLOR
 let NAVBAR_TINT = UIColor(white: 0.93, alpha: 1)
 
@@ -26,6 +29,50 @@ let CUSTOM_SESSION: URLSession = {
     config.requestCachePolicy = .reloadIgnoringLocalCacheData
     config.urlCache = nil
     config.timeoutIntervalForRequest = 5.0
-    print("setup")
     return URLSession(configuration: config)
 }()
+
+extension String {
+    
+    /// URL encode.
+    var encoded: String {
+        return self.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? self
+    }
+    
+    /// URL decode.
+    var decoded: String {
+        return self.removingPercentEncoding ?? self
+    }
+}
+
+/// Initialize new URL with url parameters
+extension URL {
+    
+    /**
+     Custom URL formatter.
+     
+     - Parameters:
+     - base: The base / parent URL of the API.
+     - API_Name: The API name.
+     - parameters: The url parameters in the form of a Swift native dictionary.
+     
+     - Returns: An optional URL object (URL?) formed from the provided information.
+     */
+    
+    static func with(base: String, API_Name: String, parameters: [String: String]) -> URL? {
+        if (parameters.isEmpty) {
+            return URL(string: base + API_Name)
+        }
+        let formattedParameters = parameters.map {
+            return $0.key.encoded + "=" + $0.value.encoded
+        }
+        return URL(string: "\(base + API_Name)?" + formattedParameters.joined(separator: "&"))
+    }
+}
+
+
+/// Documents directory URL.
+let DOCUMENTS_URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+/// Cache directory URL.
+let CACHES = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
--- Eventure/LoginViewController.swift
+++ Eventure/LoginViewController.swift
@@ -5,78 +5,277 @@
 //  Created by Xiang Li on 5/29/19.
 //  Copyright © 2019 UC Berkeley. All rights reserved.
 //
-
 import UIKit
+import SwiftyJSON
 
 class LoginViewController: UIViewController {
+    let authUsr = "eventure-frontend"
+    let authPswd = "MeiYouMiMa"
+    var canvas: UIScrollView!
     var usr = UITextField()
     var pswd = UITextField()
-    var logOrReg = UIButton(type: .system)
+    var loginButton = UIButton(type: .system)
+    var registerButton = UIButton(type: .system)
+    var activeField: UITextField?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.view.backgroundColor = MAIN_TINT
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+        self.setupCanvas()
         self.setupLogins()
+        
+        // Setup keyboard show/hide observers
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    // UI setup
+    private func setupCanvas() {
+        canvas = UIScrollView()
+        canvas.bounces = true
+        canvas.showsVerticalScrollIndicator = true
+        canvas.translatesAutoresizingMaskIntoConstraints = false
+        canvas.keyboardDismissMode = .interactive
+        view.addSubview(canvas)
+        
+        canvas.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        canvas.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        canvas.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        canvas.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    private func prepareField(textfield: UITextField) {
+        textfield.autocorrectionType = .no
+        textfield.autocapitalizationType = .none
+        textfield.clearButtonMode = .whileEditing
+        textfield.backgroundColor = .white
+        textfield.borderStyle = .roundedRect
+        let inset = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textfield.frame.height))
+        textfield.leftView = inset
+        textfield.leftViewMode = .always
     }
     
     private func setupLogins() {
-        usr.placeholder = "email"
-        usr.borderStyle = .roundedRect
-        usr.keyboardType = UIKeyboardType.default
-    
-        pswd.placeholder = "password"
-        pswd.borderStyle = .roundedRect
-        pswd.keyboardType = UIKeyboardType.default
+        usr.delegate = self
+        usr.placeholder = "Username / Email"
+        usr.keyboardType = .emailAddress
+        usr.adjustsFontSizeToFitWidth = true
+        usr.returnKeyType = .next
+        prepareField(textfield: usr)
         
-        logOrReg.setTitle("Log In / Join", for: .normal)
-        logOrReg.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize)
-        logOrReg.tintColor = .white
-        logOrReg.backgroundColor = MAIN_TINT_DARK
-        logOrReg.layer.cornerRadius = 18
+        pswd.delegate = self
+        pswd.placeholder = "Password"
+        pswd.textContentType = .password
+        pswd.isSecureTextEntry = true
+        pswd.returnKeyType = .go
+        prepareField(textfield: pswd)
         
-        view.addSubview(usr)
-        view.addSubview(pswd)
-        view.addSubview(logOrReg)
+        loginButton.setTitle("Sign In", for: .normal)
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize)
+        loginButton.tintColor = .white
+        loginButton.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
+        loginButton.layer.cornerRadius = 18
+        
+        registerButton.setTitle("Register", for: .normal)
+        registerButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        registerButton.tintColor = .white
+        registerButton.backgroundColor = .clear
+        registerButton.layer.cornerRadius = 5
+        registerButton.layer.borderColor = UIColor.white.cgColor
+        registerButton.layer.borderWidth = 2.0
+        
+        canvas.addSubview(usr)
+        canvas.addSubview(pswd)
+        canvas.addSubview(loginButton)
+        canvas.addSubview(registerButton)
         
         pswd.translatesAutoresizingMaskIntoConstraints = false
         usr.translatesAutoresizingMaskIntoConstraints = false
-        logOrReg.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
         
         //Let pswd be the center anchor
+        
         pswd.widthAnchor.constraint(equalToConstant: 210).isActive = true
         pswd.heightAnchor.constraint(equalToConstant: 45).isActive = true
-        pswd.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        pswd.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        pswd.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
+        pswd.centerYAnchor.constraint(equalTo: canvas.centerYAnchor).isActive = true
         
         usr.widthAnchor.constraint(equalToConstant: 210).isActive = true
         usr.heightAnchor.constraint(equalToConstant: 45).isActive = true
-        usr.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        usr.centerYAnchor.constraint(equalTo: pswd.centerYAnchor, constant: -50).isActive = true
+        usr.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
+        usr.centerYAnchor.constraint(equalTo: pswd.centerYAnchor,
+                                     constant: -50).isActive = true
         
-        logOrReg.widthAnchor.constraint(equalToConstant: 186).isActive = true
-        logOrReg.heightAnchor.constraint(equalToConstant: 48).isActive = true
-        logOrReg.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        logOrReg.centerYAnchor.constraint(equalTo: pswd.centerYAnchor, constant: 65).isActive = true
+        loginButton.widthAnchor.constraint(equalToConstant: 186).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        loginButton.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
+        loginButton.centerYAnchor.constraint(equalTo: pswd.centerYAnchor, constant: 65).isActive = true
+        
+        registerButton.widthAnchor.constraint(equalToConstant: 93).isActive = true
+        registerButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        registerButton.centerYAnchor.constraint(equalTo: view.bottomAnchor, constant: -25).isActive = true
+        registerButton.addTarget(self, action: #selector(registerPressed), for: .touchUpInside)
+        
         //login/register transition page
-        logOrReg.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        loginButton.addTarget(self, action: #selector(buttonLifted(_:)),
+                              for: [.touchUpOutside, .touchDragExit, .touchDragExit, .touchCancel])
+        loginButton.addTarget(self,
+                              action: #selector(beginLoginRequest),
+                              for: .touchUpInside)
+        loginButton.addTarget(self, action: #selector(buttonPressed(_:)),
+                              for: .touchDown)
+    }
+    
+    
+    // Button appearance
+    @objc private func buttonPressed(_ sender: UIButton) {
+        sender.setTitleColor(UIColor(white: 1, alpha: 0.7), for: .normal)
+        sender.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(1.0)
+    }
+    
+    @objc private func buttonLifted(_ sender: UIButton) {
+        sender.setTitleColor(.white, for: .normal)
+        sender.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
+    }
+    
+    
+    
+    @objc private func beginLoginRequest() {
+        dismissKeyboard()
+        let reset = {
+            self.loginButton.setTitleColor(.white, for: .normal)
+            self.loginButton.setTitle("Sign In", for: .normal)
+            self.loginButton.isEnabled = true
+            self.loginButton.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
+        }
+        
+        // First verifies that the username and password are not blank
+        guard let username = usr.text, username != "" else {
+            print("username is empty")
+            reset()
+            return
+        }
+        
+        guard let password = pswd.text, password != "" else {
+            print("password is empty")
+            reset()
+            return
+        }
+        
+        // Change the button style to reflect that a login request is in progress
+        loginButton.isEnabled = false
+        loginButton.setTitleColor(UIColor(white: 1, alpha: 0.7), for: .normal)
+        loginButton.setTitle("Signing In...", for: .normal)
+        loginButton.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(1.0)
+        
+        // Construct the URL parameters to be delivered
+        let loginParameters = [
+            "login": username,
+            "password": password
+        ]
+        
+        // Make the URL and URL request
+        let apiURL = URL.with(base: API_BASE_URL,
+                              API_Name: "account/Authenticate",
+                              parameters: loginParameters)!
+        var request = URLRequest(url: apiURL)
+        let token = "\(authUsr):\(authPswd)".data(using: .utf8)!.base64EncodedString()
+        request.httpMethod = "POST"
+        request.addValue("Basic \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = CUSTOM_SESSION.dataTask(with: request) {
+            data, response, error in
+            
+            DispatchQueue.main.async {
+                reset()
+            }
+            
+            guard error == nil else {
+                print(error!); return
+            }
+            
+            do {
+                let result = try JSON(data: data!).dictionary
+                let servermsg = result?["status"]?.rawString()
+                print(servermsg!)
+                if (servermsg == "success") {
+                    let nextVC = MainTabBarController()
+                    self.present(nextVC, animated: true, completion: nil)
+                } else {
+                    //UI related events belong in main thread
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Login Error", message: servermsg, preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            } catch {
+                print("error parsing")
+            }
+        }
+        task.resume()
         
     }
-    @objc private func buttonPressed() {
-        //TODO: NEED API here
-        //print(sender.text)
-        let nextVC = MainTabBarController()
-        //nextVC.modalTransitionStyle = .coverVertical
+    
+    @objc private func registerPressed() {
+        let nextVC = RegisterViewController()
         self.present(nextVC, animated: true, completion: nil)
     }
-    @objc private func dismissKeyboard() {
-        if (usr.isEditing) {
-            print("usr")
-            usr.resignFirstResponder()
-        } else if (pswd.isEditing) {
-            print("pswd")
-            pswd.resignFirstResponder()
+    // Notification handlers to make sure that the active textfield is always visible
+
+    @objc private func keyboardDidShow(_ notification: Notification) {
+        let kbSize = ((notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey]) as! CGRect).size
+        canvas.contentInset.bottom = kbSize.height
+        canvas.scrollIndicatorInsets.bottom = kbSize.height
+        if let textfield = activeField {
+            // Determine whether additional space is needed to fully display the keyboard
+            let bottomSpace = max(0, kbSize.height - canvas.frame.height +  textfield.frame.maxY + 8)
+            canvas.contentOffset.y = bottomSpace
         }
     }
     
+    @objc private func keyboardDidHide(_ notification: Notification) {
+        canvas.contentInset = .zero
+        canvas.scrollIndicatorInsets = .zero
+        canvas.contentOffset.y = 0
+    }
+    
+    @objc private func dismissKeyboard() {
+        canvas.endEditing(true)
+    }
+    
+    
+}
+
+
+// Add editing events detection
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeField = nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == usr) {
+            pswd.becomeFirstResponder()
+        } else {
+            self.dismissKeyboard()
+        }
+        return true
+    }
 }
