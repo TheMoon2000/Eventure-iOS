cf76b870cb3e222af26ec20caf9f14f7e36f73cb
Victor_Li <lytz@berkeley.edu>
2019-06-04 12:29:09 -0700

    register basic functionality and login page gradient background and logo addition to login page and other minor code optimization

--- Eventure/Global Constants.swift
+++ Eventure/Global Constants.swift
@@ -20,6 +20,33 @@ let MAIN_TINT2 = UIColor(red: 0.5, green: 0.7, blue: 0.92, alpha: 1)
 let MAIN_TINT = UIColor(red: 127/255, green: 219/255, blue: 203/255, alpha: 1.0)
 let MAIN_TINT_DARK = UIColor(red: 73/255, green: 188/255, blue: 167/255, alpha: 1)
 
+let MAIN_TINT3 = UIColor(red: 133/255, green: 215/255, blue: 205/255, alpha: 1.0)
+let MAIN_TINT4 = UIColor(red: 28/255, green: 239/255, blue: 255/255, alpha: 1.0)
+
+let MAIN_TINT5 = UIColor(red: 0/255, green: 203/255, blue: 223/255, alpha: 1.0)
+let MAIN_TINT6 = UIColor(red: 236/255, green: 110/255, blue: 173/255, alpha: 1.0)
+let MAIN_TINT7 = UIColor(red: 52/255, green: 147/255, blue: 230/255, alpha: 1.0)
+let MAIN_TINT8 = UIColor(red: 255/255, green: 153/255, blue: 102/255, alpha: 1.0)
+let MAIN_TINT9 = UIColor(red: 255/255, green: 94/255, blue: 98/255, alpha: 1.0)
+
+let MAIN_TINT10 = UIColor(red: 255/255, green: 128/255, blue: 8/255, alpha: 1.0)
+let MAIN_TINT11 = UIColor(red: 255/255, green: 200/255, blue: 55/255, alpha: 1.0)
+
+let MAIN_TINT12 = UIColor(red: 255/255, green: 81/255, blue: 47/255, alpha: 1.0)
+let MAIN_TINT13 = UIColor(red: 240/255, green: 152/255, blue: 25/255, alpha: 1.0)
+
+let MAIN_TINT14 = UIColor(red: 242/255, green: 153/255, blue: 74/255, alpha: 1.0)
+let MAIN_TINT15 = UIColor(red: 242/255, green: 201/255, blue: 76/255, alpha: 1.0)
+
+let MAIN_TINT16 = UIColor(red: 255/255, green: 179/255, blue: 71/255, alpha: 1.0)
+let MAIN_TINT17 = UIColor(red: 255/255, green: 204/255, blue: 51/255, alpha: 1.0)
+
+let MAIN_TINT18 = UIColor(red: 103/255, green: 178/255, blue: 111/255, alpha: 1.0)
+let MAIN_TINT19 = UIColor(red: 76/255, green: 162/255, blue: 205/255, alpha: 1.0)
+
+let MAIN_TINT20 = UIColor(red: 118/255, green: 184/255, blue: 82/255, alpha: 1.0)
+let MAIN_TINT21 = UIColor(red: 141/255, green: 194/255, blue: 111/255, alpha: 1.0)
+
 /// Todo: REPLACE THIS WITH THE NAVIGATION BAR COLOR
 let NAVBAR_TINT = UIColor(white: 0.93, alpha: 1)
 
@@ -70,7 +97,13 @@ extension URL {
     }
 }
 
-
+extension UITextField {
+    func doInset() {
+        var inset = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = inset
+        self.leftViewMode = .always
+    }
+}
 /// Documents directory URL.
 let DOCUMENTS_URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
 
--- Eventure/RegisterViewController.swift
+++ Eventure/RegisterViewController.swift
@@ -7,23 +7,252 @@
 //
 
 import UIKit
-
-class RegisterViewController: UIViewController {
+import SwiftyJSON
+/*
+struct cellData {
+    var blank : String
+    var info : UITextField
+}*/
+class RegisterViewController: UITableViewController {
+    let authUsr = "eventure-frontend"
+    let authPswd = "MeiYouMiMa"
+    var front2back = ["Email": "email", "Name": "displayedName", "Password": "password", "Gender": "gender"]
+    var loginParameters = ["email": "", "displayedName": "", "password": "", "gender": "", "date": ""]
+    let blanks = ["Email", "Name", "Password", "Gender"]
+    var inputs : Array<UITextField?>!
+    var activeField: UITextField?
+    var register = UIButton(type: .system)
+    var foot : UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inputs = Array(repeatElement(nil, count: blanks.count))
         
         self.view.backgroundColor = .white
         let g = UISwipeGestureRecognizer(target: self, action: #selector(returnToLogin))
         g.direction = UISwipeGestureRecognizer.Direction.right
         self.view.addGestureRecognizer(g)
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
         self.navigationController?.setNavigationBarHidden(false, animated: false)
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        
+        setupTable()
+        setupUI()
+    }
+    private func setupTable() {
+        self.tableView.separatorStyle = .none
+        self.tableView.register(RegisterCell.self, forCellReuseIdentifier: "register")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        //TODO: Resize
+        foot = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 500))
+        self.tableView.tableFooterView = foot
+        foot.backgroundColor = .white
+        
+    }
+    
+    private func setupUI() {
+        //self.tableView.tableFooterView!.addSubview(register)
+        foot.addSubview(register)
+        
+        register.setTitle("Register", for: .normal)
+        register.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize)
+        register.tintColor = .white
+        register.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
+        register.layer.cornerRadius = 18
+        
+        register.translatesAutoresizingMaskIntoConstraints = false
+        register.widthAnchor.constraint(equalToConstant: 186).isActive = true
+        register.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        register.centerXAnchor.constraint(equalTo: foot.centerXAnchor).isActive = true
+        register.centerYAnchor.constraint(equalTo: foot.bottomAnchor, constant: -45).isActive = true
+        
+        register.addTarget(self, action: #selector(buttonLifted(_:)),
+                              for: [.touchUpOutside, .touchDragExit, .touchDragExit, .touchCancel])
+        register.addTarget(self,
+                              action: #selector(sendRegistration),
+                              for: .touchUpInside)
+        register.addTarget(self, action: #selector(buttonPressed(_:)),
+                              for: .touchDown)
+        
+    }
+    @objc private func buttonPressed(_ sender: UIButton) {
+        sender.setTitleColor(UIColor(white: 1, alpha: 0.7), for: .normal)
+        sender.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(1.0)
+    }
+    
+    @objc private func buttonLifted(_ sender: UIButton) {
+        sender.setTitleColor(.white, for: .normal)
+        sender.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
+    }
+    
+    @objc private func sendRegistration() {
+        dismissKeyboard()
+
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd" // e.g. 2019-06-02
+        df.locale = Locale(identifier: "en_US")
+        let date = df.string(from: Date()) // pass this to the URL parameters
+        loginParameters["date"] = date
+        
+        let reset = {
+            self.register.setTitleColor(.white, for: .normal)
+            self.register.setTitle("Register", for: .normal)
+            self.register.isEnabled = true
+            self.register.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
+        }
+        let cells = self.tableView.visibleCells as! [RegisterCell]
+        
+        //storing user inputs
+        var shouldEnd = false
+        for c in cells {
+            let field = c.info
+            if (field.text == nil || field.text == "") {
+                shouldEnd = true
+                print(front2back[c.blank.text!]! + " is empty")
+                field.layer.borderColor = UIColor.magenta.cgColor
+                reset()
+            } else {
+                loginParameters[front2back[c.blank.text!]!] = field.text
+            }
+        }
+        if (shouldEnd) {
+            return
+        }
+        print(loginParameters)
+        //Make the URL and URL request
+        let apiURL = URL.with(base: API_BASE_URL,
+                              API_Name: "account/Register",
+                              parameters: loginParameters)!
+        var request = URLRequest(url: apiURL)
+        
+        let token = "\(authUsr):\(authPswd)".data(using: .utf8)!.base64EncodedString()
+        request.httpMethod = "POST"
+        request.addValue("Basic \(token)", forHTTPHeaderField: "Authorization")
+        let task = CUSTOM_SESSION.dataTask(with: request) {
+            data, response, error in
+            
+            DispatchQueue.main.async {
+                reset()
+            }
+            
+            guard error == nil else {
+                print(error!); return
+            }
+            
+            do {
+                print(String(data: data!, encoding: .ascii)!)
+                print(1)
+                let result = try JSON(data: data!).dictionary
+                let servermsg = result?["status"]?.rawString()
+                print(servermsg!)
+                if (servermsg == "success") {
+                    let nextVC = MainTabBarController()
+                    self.navigationController?.pushViewController(nextVC, animated: true)
+                } else {
+                    //UI related events belong in main thread
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Register Error", message: servermsg, preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            } catch {
+                print("error parsing")
+            }
+        }
+        task.resume()
+        
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "register") as! RegisterCell
+        cell.blank.text = blanks[indexPath.row]
+        //do not initialize blank or info here for it has passed the super init stage for a cell
+        inputs[indexPath.row] = cell.info
+        if (indexPath.row == 0) {
+            cell.info.keyboardType = .emailAddress
+        }
+        if (indexPath.row < inputs.count - 1) {
+            cell.info.returnKeyType = .next
+        } else if (indexPath.row == inputs.count - 1) {
+            cell.info.returnKeyType = .done
+        }
+        cell.info.delegate = self
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return blanks.count
     }
     
     @objc private func returnToLogin() {
         //TODO: Use navigation controller for better animation
         self.navigationController?.popViewController(animated: true)
     }
+    
+    @objc private func keyboardDidShow(_ notification: Notification) {
+        let kbSize = ((notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey]) as! CGRect).size
+        self.tableView.contentInset.bottom = kbSize.height
+        self.tableView.scrollIndicatorInsets.bottom = kbSize.height
+        if let textfield = activeField {
+            // Determine whether additional space is needed to fully display the keyboard
+            let bottomSpace = max(0, kbSize.height - self.tableView.frame.height +  textfield.frame.maxY + 8)
+            self.tableView.contentOffset.y = bottomSpace
+        }
+    }
+    @objc private func keyboardDidHide(_ notification: Notification) {
+        self.tableView.contentInset = .zero
+        self.tableView.scrollIndicatorInsets = .zero
+        self.tableView.contentOffset.y = 0
+    }
+    
+    @objc private func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
 }
 
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.black.cgColor
+        activeField = textField as UITextField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeField = nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        for i in (0..<inputs.count) {
+            if (textField == inputs[i] && i < inputs.count - 1) {
+                inputs[i + 1]?.becomeFirstResponder()
+                //you have to return true here since otherwise
+                //it would continue the for loop and
+                //either dismiss or assign the wrong first reponder
+                return true
+            } else {
+                self.dismissKeyboard()
+            }
+        }
+        return true
+    }
+}
+
+class regLabel : UILabel {
+    override open func draw(_ rect: CGRect) {
+        let inset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        self.drawText(in: rect.inset(by: inset))
+    }
+}
