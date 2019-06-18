93ac6689af2b4eb68e71b43ed877e4e6125f67af
Victor_Li <lytz@berkeley.edu>
2019-06-18 23:07:15 +0200

    basic forgot password UI and registration then login now works

--- Eventure/ForgotPSWDViewController.swift
+++ Eventure/ForgotPSWDViewController.swift
@@ -1,32 +1,25 @@
 //
-//  LoginViewController.swift
+//  ForgotPSWDViewController.swift
 //  Eventure
 //
-//  Created by Xiang Li on 5/29/19.
+//  Created by Xiang Li on 6/18/19.
 //  Copyright © 2019 UC Berkeley. All rights reserved.
 //
+
 import UIKit
 import SwiftyJSON
 
-
-
-class LoginViewController: UIViewController {
-    let authUsr = "eventure-frontend"
-    let authPswd = "MeiYouMiMa"
+class ForgotPSWDViewController: UIViewController {
     var canvas: UIScrollView!
-    var usr = UITextField()
-    var pswd = UITextField()
-    var loginButton = UIButton(type: .system)
-    var registerButton = UIButton(type: .system)
-    var forgotButton = UIButton(type: .system)
+    var forgot = UITextField()
+    var retrieveButton = UIButton(type: .system)
     var activeField: UITextField?
-    var logo = UIImageView()
-    
-    
+    let authforgot = "eventure-frontend"
+    let authPswd = "MeiYouMiMa"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        //self.view.backgroundColor = MAIN_TINT3
+        
         let topColor = MAIN_TINT8
         let buttomColor = MAIN_TINT6
         let gradientColors = [topColor.cgColor, buttomColor.cgColor]
@@ -44,6 +37,8 @@ class LoginViewController: UIViewController {
         self.setupCanvas()
         self.setupLogins()
         
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
         // Setup keyboard show/hide observers
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(keyboardDidShow(_:)),
@@ -56,6 +51,8 @@ class LoginViewController: UIViewController {
         
         //to detect rotation change and any kind of unexpected UI changes/lack of changes
         NotificationCenter.default.addObserver(self, selector: #selector(rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
+
+        // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: false)
@@ -103,108 +100,50 @@ class LoginViewController: UIViewController {
     }
     
     private func setupLogins() {
-        usr.delegate = self
-        usr.placeholder = "Username / Email"
-        usr.keyboardType = .emailAddress
-        usr.adjustsFontSizeToFitWidth = true
-        usr.returnKeyType = .next
-        prepareField(textfield: usr)
+        forgot.delegate = self
+        forgot.placeholder = "Enter Email for Password"
+        forgot.keyboardType = .emailAddress
+        forgot.adjustsFontSizeToFitWidth = true
+        forgot.returnKeyType = .next
+        prepareField(textfield: forgot)
         
-        pswd.delegate = self
-        pswd.placeholder = "Password"
-        pswd.textContentType = .password
-        pswd.isSecureTextEntry = true
-        pswd.returnKeyType = .go
-        prepareField(textfield: pswd)
+        retrieveButton.setTitle("Send Email", for: .normal)
+        retrieveButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        retrieveButton.tintColor = .white
+        retrieveButton.backgroundColor = .clear
+        retrieveButton.layer.cornerRadius = 5
+        retrieveButton.layer.borderColor = UIColor.white.cgColor
+        retrieveButton.layer.borderWidth = 2.0
         
-        loginButton.setTitle("Sign In", for: .normal)
-        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize)
-        loginButton.tintColor = .white
-        //loginButton.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
-        loginButton.backgroundColor = .clear
-        loginButton.layer.cornerRadius = 18
-        //delete these
-        loginButton.layer.borderColor = UIColor.white.cgColor
-        loginButton.layer.borderWidth = 2.0
+        canvas.addSubview(forgot)
+        canvas.addSubview(retrieveButton)
         
-        registerButton.setTitle("Register", for: .normal)
-        registerButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
-        registerButton.tintColor = .white
-        registerButton.backgroundColor = .clear
-        registerButton.layer.cornerRadius = 5
-        registerButton.layer.borderColor = UIColor.white.cgColor
-        registerButton.layer.borderWidth = 2.0
-        
-        let forgotTitle =  NSAttributedString(string: "Forgot Password",
-                                              attributes: [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue, NSAttributedString.Key.font: UIFont.systemFont(ofSize: UIFont.systemFontSize - 3)])
-        forgotButton.setAttributedTitle(forgotTitle, for: .normal)
-        forgotButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize)
-        forgotButton.tintColor = .white
-        forgotButton.backgroundColor = .clear
-        
-        
-        logo.image = UIImage(named: "logo")
-
-        canvas.addSubview(usr)
-        canvas.addSubview(pswd)
-        canvas.addSubview(loginButton)
-        canvas.addSubview(registerButton)
-        canvas.addSubview(forgotButton)
-        canvas.addSubview(logo)
-        
-        logo.frame = CGRect(x: 0, y: 0, width: 50, height: 30)
-        logo.contentMode = .scaleAspectFit
-        logo.clipsToBounds = true
-        logo.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        logo.heightAnchor.constraint(equalToConstant: 250).isActive = true
-        logo.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
-        logo.bottomAnchor.constraint(equalTo: usr.topAnchor, constant: 50).isActive = true
-        
-        pswd.translatesAutoresizingMaskIntoConstraints = false
-        usr.translatesAutoresizingMaskIntoConstraints = false
-        loginButton.translatesAutoresizingMaskIntoConstraints = false
-        registerButton.translatesAutoresizingMaskIntoConstraints = false
-        forgotButton.translatesAutoresizingMaskIntoConstraints = false
-        logo.translatesAutoresizingMaskIntoConstraints = false
+        forgot.translatesAutoresizingMaskIntoConstraints = false
+        retrieveButton.translatesAutoresizingMaskIntoConstraints = false
         
         //Let pswd be the center anchor
         
-        pswd.widthAnchor.constraint(equalToConstant: 210).isActive = true
-        pswd.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        pswd.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
-        pswd.centerYAnchor.constraint(equalTo: canvas.centerYAnchor).isActive = true
-        
-        usr.widthAnchor.constraint(equalToConstant: 210).isActive = true
-        usr.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        usr.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
-        usr.centerYAnchor.constraint(equalTo: pswd.centerYAnchor,
+        forgot.widthAnchor.constraint(equalToConstant: 210).isActive = true
+        forgot.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        forgot.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
+        forgot.centerYAnchor.constraint(equalTo: canvas.centerYAnchor,
                                      constant: -50).isActive = true
         
-        loginButton.widthAnchor.constraint(equalToConstant: 210).isActive = true
-        loginButton.heightAnchor.constraint(equalToConstant: 42).isActive = true
-        loginButton.centerXAnchor.constraint(equalTo: canvas.centerXAnchor).isActive = true
-        loginButton.centerYAnchor.constraint(equalTo: pswd.centerYAnchor, constant: 65).isActive = true
         
-        registerButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        registerButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        registerButton.centerYAnchor.constraint(equalTo: view.bottomAnchor, constant: -45).isActive = true
-        registerButton.addTarget(self, action: #selector(registerPressed), for: .touchUpInside)
+        retrieveButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        retrieveButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        retrieveButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        retrieveButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 45).isActive = true
         
-        forgotButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        forgotButton.heightAnchor.constraint(equalToConstant: 15).isActive = true
-        forgotButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        forgotButton.centerYAnchor.constraint(equalTo: view.bottomAnchor, constant: -15).isActive = true
         
         //login/register transition page
-        loginButton.addTarget(self, action: #selector(buttonLifted(_:)),
+        retrieveButton.addTarget(self, action: #selector(buttonLifted(_:)),
                               for: [.touchUpOutside, .touchDragExit, .touchDragExit, .touchCancel])
-        loginButton.addTarget(self,
+        retrieveButton.addTarget(self,
                               action: #selector(beginLoginRequest),
                               for: .touchUpInside)
-        loginButton.addTarget(self, action: #selector(buttonPressed(_:)),
+        retrieveButton.addTarget(self, action: #selector(buttonPressed(_:)),
                               for: .touchDown)
-        forgotButton.addTarget(self, action: #selector(forgotPSWD), for: .touchDown)
     }
     
     
@@ -217,50 +156,34 @@ class LoginViewController: UIViewController {
     @objc private func buttonLifted(_ sender: UIButton) {
         sender.setTitleColor(.white, for: .normal)
         sender.backgroundColor = .clear
-        //sender.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
-    }
-    
-    @objc private func forgotPSWD(_ sender: UIButton) {
-        let nextVC = ForgotPSWDViewController()
-        self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
     @objc private func beginLoginRequest() {
         dismissKeyboard()
         let reset = {
-            self.loginButton.setTitleColor(.white, for: .normal)
-            self.loginButton.setTitle("Sign In", for: .normal)
-            self.loginButton.isEnabled = true
-            self.loginButton.backgroundColor = .clear
-            //self.loginButton.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(0.8)
+            self.retrieveButton.setTitleColor(.white, for: .normal)
+            self.retrieveButton.setTitle("Send Email", for: .normal)
+            self.retrieveButton.isEnabled = true
+            self.retrieveButton.backgroundColor = .clear
         }
         
         // First verifies that the username and password are not blank
-        guard let username = usr.text, username != "" else {
+        guard let username = forgot.text, username != "" else {
             print("username is empty")
-            usr.shake()
-            reset()
-            return
-        }
-        
-        guard let password = pswd.text, password != "" else {
-            print("password is empty")
-            pswd.shake()
+            forgot.shake()
             reset()
             return
         }
         
         // Change the button style to reflect that a login request is in progress
-        loginButton.isEnabled = false
-        loginButton.setTitleColor(UIColor(white: 1, alpha: 0.7), for: .normal)
-        loginButton.setTitle("Signing In...", for: .normal)
-        loginButton.backgroundColor = .clear
-        //loginButton.backgroundColor = MAIN_TINT_DARK.withAlphaComponent(1.0)
+        retrieveButton.isEnabled = false
+        retrieveButton.setTitleColor(UIColor(white: 1, alpha: 0.7), for: .normal)
+        retrieveButton.setTitle("Sending...", for: .normal)
+        retrieveButton.backgroundColor = .clear
         
         // Construct the URL parameters to be delivered
         let loginParameters = [
-            "email": username,
-            "password": password
+            "email": username
         ]
         
         // Make the URL and URL request
@@ -268,8 +191,8 @@ class LoginViewController: UIViewController {
                               API_Name: "account/Authenticate",
                               parameters: loginParameters)!
         var request = URLRequest(url: apiURL)
-
-        let token = "\(authUsr):\(authPswd)".data(using: .utf8)!.base64EncodedString()
+        
+        let token = "\(authforgot):\(authPswd)".data(using: .utf8)!.base64EncodedString()
         request.httpMethod = "POST"
         request.addValue("Basic \(token)", forHTTPHeaderField: "Authorization")
         
@@ -309,12 +232,8 @@ class LoginViewController: UIViewController {
         
     }
     
-    @objc private func registerPressed() {
-        let nextVC = RegisterViewController()
-        self.navigationController?.pushViewController(nextVC, animated: true)
-    }
     // Notification handlers to make sure that the active textfield is always visible
-
+    
     @objc private func keyboardDidShow(_ notification: Notification) {
         let kbSize = ((notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey]) as! CGRect).size
         canvas.contentInset.bottom = kbSize.height
@@ -335,12 +254,19 @@ class LoginViewController: UIViewController {
         canvas.endEditing(true)
     }
 }    
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 
-// Add editing events detection
-extension LoginViewController: UITextFieldDelegate {
-    
+extension ForgotPSWDViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         activeField = textField
     }
@@ -348,23 +274,8 @@ extension LoginViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         activeField = nil
     }
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if (textField == usr) {
-            pswd.becomeFirstResponder()
-        } else {
-            self.dismissKeyboard()
-
-        }
+        self.dismissKeyboard()
         return true
-    }
-}
-extension UIView {
-    func shake() {
-        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-        animation.duration = 0.5
-        animation.values = [-10.0, 10.0, -10.0, 10.0, -5.0, 5.0, -2.5, 2.5, 0.0 ]
-        layer.add(animation, forKey: "shake")
     }
 }
