51d73c6df1f4b1ddd0b145afaefd6b11a38febd8
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-06-19 14:04:18 -0400

    Updated registration page

---
+++ Eventure/Login/FinishRegistration.swift
@@ -0,0 +1,191 @@
+//
+//  FinishRegistration.swift
+//  Eventure
+//
+//  Created by Jia Rui Shan on 2019/6/19.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class FinishRegistration: UIViewController {
+    
+    var parameters: [String : String]?
+    var regVC: RegisterController?
+    
+    private var spinner: UIActivityIndicatorView!
+    private var spinnerCaption: UILabel!
+    private var button: UIButton!
+    private var completionImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        
+        spinner = makeSpinner()
+        spinner.startAnimating()
+
+        spinnerCaption = makeLabel()
+        
+        button = makeButton()
+        button.isHidden = true
+        
+        completionImage = makeImageView()
+        
+        createAccount()
+    }
+    
+    private func makeSpinner() -> UIActivityIndicatorView {
+        let spinner = UIActivityIndicatorView(style: .whiteLarge)
+        spinner.color = UIColor(white: 0.75, alpha: 1)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(spinner)
+        
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor,
+                                         constant: -25).isActive = true
+        return spinner
+    }
+    
+    private func makeLabel() -> UILabel {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 2
+        label.text = "Creating your Account..."
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = UIColor(white: 0.3, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        label.topAnchor.constraint(equalTo: spinner.bottomAnchor,
+                                   constant: 24).isActive = true
+        label.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor,
+                                    constant: 30).isActive = true
+        label.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor,
+                                     constant: -30).isActive = true
+        
+        
+        return label
+    }
+    
+    private func makeButton() -> UIButton {
+        let button = UIButton(type: .system)
+        button.tintColor = MAIN_TINT_DARK
+        button.titleLabel?.font = .systemFont(ofSize: 19, weight: .medium)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        
+        button.widthAnchor.constraint(equalToConstant: 220).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                       constant: -29).isActive = true
+        
+        button.addTarget(self, action: #selector(returnToLogin), for: .touchUpInside)
+        
+        return button
+    }
+    
+    private func makeImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        imageView.widthAnchor.constraint(equalToConstant: 52).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 56).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: spinner.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: spinner.centerYAnchor).isActive = true
+        
+        return imageView
+    }
+    
+    
+    @objc private func returnToLogin(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        
+        if sender.title(for: .normal) == "Return to Login" {
+            self.regVC?.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    private func failed(msg: String) {
+        spinner.stopAnimating()
+        spinnerCaption.text = msg
+        button.isHidden = false
+        button.setTitle("Return to Registration", for: .normal)
+        completionImage.image = #imageLiteral(resourceName: "error")
+    }
+    
+    private func succeeded() {
+        spinner.stopAnimating()
+        spinnerCaption.text = "Your Eventure account was created!"
+        button.isHidden = false
+        button.setTitle("Return to Login", for: .normal)
+        completionImage.image = #imageLiteral(resourceName: "done")
+    }
+    
+    private func createAccount() {
+        
+        guard var parameters = self.parameters else {
+            return
+        }
+        
+        if parameters["displayedName"] == nil {
+            parameters["displayedName"] = parameters["email"]
+        }
+        
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd" // e.g. 2019-06-02
+        df.locale = Locale(identifier: "en_US")
+        let dateStr = df.string(from: Date()) // pass this to the URL parameters
+        parameters["date"] = dateStr
+        
+        print(parameters) // The finalized URL parameters
+        
+        let url = URL.with(base: API_BASE_URL,
+                           API_Name: "account/Register",
+                           parameters: parameters)!
+        
+        var request = URLRequest(url: url)
+        
+        // Authentication
+        let token = "\(USERNAME):\(PASSWORD)".data(using: .utf8)!.base64EncodedString()
+        request.addValue("Basic \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = CUSTOM_SESSION.dataTask(with: request) {
+            data, response, error in
+            
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            if let str = String(data: data!, encoding: .ascii) {
+                print(str)
+                DispatchQueue.main.async {
+                    switch str {
+                    case "success":
+                        self.succeeded()
+                    case "invalid email":
+                        self.failed(msg: "The provided email was invalid.")
+                    case "internal error":
+                        self.failed(msg: "The server is temporarily unavailable.")
+                    case "email exists":
+                        self.failed(msg: "The provided email is already registered.")
+                    default:
+                        self.failed(msg: "An unknown error has occurred.")
+                    }
+                }
+                
+            }
+        }
+        
+        task.resume()
+        
+    }
+
+}
---
+++ Eventure/RegisterController.swift
@@ -0,0 +1,343 @@
+//
+//  RegisterController.swift
+//  Eventure
+//
+//  Created by Jia Rui Shan on 2019/6/18.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+        
+    private var showingPicker = false
+    private var picker: UIPickerView?
+    private var signupButton: UIBarButtonItem?
+    private(set) var parameters = [String: String]() {
+        didSet {
+            var valid = true
+            for item in ["email", "password", "gender"] {
+                valid = valid && parameters.keys.contains(item)
+            }
+            signupButton?.isEnabled = valid
+        }
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // We want a grouped table view for better clarity to the user
+        tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.keyboardDismissMode = .interactive
+        
+        tableView.contentInset.top = 10.0
+        
+        self.title = "Register Account"
+        view.backgroundColor = UIColor(white: 0.97, alpha: 1) // REPLACE
+        
+        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(close))
+        self.navigationItem.leftBarButtonItem = cancel
+        signupButton = UIBarButtonItem(title: "Sign Up", style: .done, target: self, action: #selector(signup))
+        signupButton?.isEnabled = false
+        self.navigationItem.rightBarButtonItem = signupButton
+        self.navigationController?.navigationBar.tintColor = MAIN_TINT_DARK
+    }
+    
+    @objc private func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func signup(_ sender: UIBarButtonItem) {
+        let finishRegVC = FinishRegistration()
+        finishRegVC.regVC = self
+        finishRegVC.parameters = parameters
+        self.present(finishRegVC, animated: true, completion: nil)
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return [3, 3][section]
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return ["Login Credentials", "Profile"][section]
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return ["Password must be at least 8 characters long.", "Information in this section will be visible to other users."][section]
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if (indexPath.section == 1 && indexPath.row == 2) {
+            return showingPicker ? 216 : 0
+        } else {
+            return super.tableView(tableView, heightForRowAt: indexPath)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            let cell = EditableTextCell()
+            cell.textField.placeholder = "Email Address"
+            cell.textField.keyboardType = .emailAddress
+            cell.textField.autocorrectionType = .no
+            cell.completionHandler = {
+                cell.status = .loading
+                self.verifyEmail(cell: cell)
+            }
+            cell.returnHandler = {
+                if let nextCell = tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? EditableTextCell {
+                    nextCell.textField.becomeFirstResponder()
+                }
+            }
+            cell.changeHandler = {
+                self.verifyEmail(cell: cell, editing: true)
+            }
+            
+            return cell
+        case (0, 1):
+            let cell = EditableTextCell()
+            cell.textField.isSecureTextEntry = true
+            cell.textField.placeholder = "Password"
+            cell.completionHandler = {
+                self.verifyPassword(cell: cell)
+            }
+            cell.returnHandler = {
+                if let nextCell = tableView.cellForRow(at: IndexPath(row: 2, section: 0)) as? EditableTextCell {
+                    nextCell.textField.becomeFirstResponder()
+                }
+            }
+            cell.changeHandler = {
+                self.verifyPassword(cell: cell, editing: true)
+            }
+            
+            return cell
+        case (0, 2):
+            let cell = EditableTextCell()
+            cell.textField.isSecureTextEntry = true
+            cell.textField.placeholder = "Repeat Password"
+            cell.completionHandler = {
+                if let lastCell = tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? EditableTextCell {
+                    self.verifyPasswords(cell: lastCell, cell2: cell)
+                }
+            }
+            cell.returnHandler = {
+                if let nextCell = tableView.cellForRow(at: IndexPath(row: 0, section: 1)) as? EditableTextCell {
+                    nextCell.textField.becomeFirstResponder()
+                }
+            }
+            cell.changeHandler = {
+                if let lastCell = tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? EditableTextCell {
+                    self.verifyPasswords(cell: lastCell,
+                                         cell2: cell,
+                                         editing: true)
+                }
+            }
+            
+            return cell
+        case (1, 0):
+            let cell = EditableTextCell()
+            cell.textField.placeholder = "Display Name"
+            cell.textField.autocorrectionType = .no
+            cell.textField.autocapitalizationType = .words
+            cell.textField.returnKeyType = .done
+            cell.status = .info
+            cell.returnHandler = {
+                cell.textField.endEditing(true)
+            }
+            cell.changeHandler = {
+                self.parameters["displayedName"] = cell.textField.text!.isEmpty ? nil : cell.textField.text!
+            }
+            
+            return cell
+        case (1, 1):
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+            cell.textLabel?.text = "Gender"
+            cell.detailTextLabel?.text = "Unspecified"
+            parameters["gender"] = "-1"
+            cell.accessoryType = .disclosureIndicator
+            
+            return cell
+        case (1, 2):
+            let cell = UITableViewCell()
+            let picker = UIPickerView()
+            picker.dataSource = self
+            picker.delegate = self
+            picker.showsSelectionIndicator = true
+            self.picker = picker
+            
+            picker.translatesAutoresizingMaskIntoConstraints = false
+            cell.addSubview(picker)
+            
+            picker.centerXAnchor.constraint(equalTo: cell.centerXAnchor).isActive = true
+            picker.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
+            picker.widthAnchor.constraint(equalTo: cell.widthAnchor).isActive = true
+            picker.heightAnchor.constraint(equalTo: cell.heightAnchor).isActive = true
+            
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+ 
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        case (1, 0):
+            let alert = UIAlertController(title: "Display Name", message: "This is the name that other users will see from you. It doesn't need to be unique. If you leave this field blank, your email address will be used instead.", preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        default:
+            break
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1 && indexPath.section == 1 {
+            view.endEditing(true) // dismiss the keyboard
+            showingPicker = !showingPicker
+            
+            UIView.animate(withDuration: 0.25) {
+                tableView.reloadRows(at: [IndexPath(row: 2, section: 1)], with: .fade)
+            }
+            
+            if showingPicker {
+                let row = parameters["gender"] ?? "-1"
+                picker?.selectRow(Int(row)! + 1, inComponent: 0, animated: false)
+            }
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
+    private func verifyEmail(cell: EditableTextCell, editing: Bool = false) {
+        
+        self.parameters["email"] = nil
+        
+        if editing {
+            cell.status = .none
+            return
+        }
+
+        func isValidEmail(_ string: String) -> Bool {
+            // here, `try!` will always succeed because the pattern is valid
+            let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+            return regex.firstMatch(in: string, options: [], range: NSRange(location: 0, length: string.count)) != nil
+        }
+        
+        guard isValidEmail(cell.textField.text!) else {
+            cell.status = editing ? .none : .fail
+            return
+        }
+
+        let parameters = ["email": cell.textField.text ?? ""]
+        let url = URL.with(base: API_BASE_URL,
+                           API_Name: "account/GetUserInfo",
+                           parameters: parameters)!
+        
+        var request = URLRequest(url: url)
+        
+        // Authentication
+        let token = "\(USERNAME):\(PASSWORD)".data(using: .ascii)!.base64EncodedString()
+        request.addValue("Basic \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = CUSTOM_SESSION.dataTask(with: request) {
+            data, response, error in
+            
+            guard error == nil else {
+                print(error!)
+                DispatchQueue.main.async { cell.status = .disconnected }
+                return
+            }
+            
+            if let str = String(data: data!, encoding: .ascii) {
+                DispatchQueue.main.async {
+                    cell.status = str == "not found" ? .tick : .fail
+                    if str == "not found" {
+                        cell.status = .tick
+                        self.parameters["email"] = cell.textField.text!
+                    } else {
+                        cell.status = .fail
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    cell.status = .disconnected
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func verifyPassword(cell: EditableTextCell, editing: Bool = false) {
+        self.parameters["password"] = nil
+        if cell.textField.text!.count >= 8 {
+            let nextCell = tableView.cellForRow(at: IndexPath(row: 2, section: 0)) as! EditableTextCell
+            
+            if nextCell.textField.text == cell.textField.text {
+                cell.status = .tick
+                nextCell.status = .tick
+                self.parameters["password"] = cell.textField.text
+            } else if editing {
+                cell.status = .none
+            } else if !nextCell.textField.text!.isEmpty {
+                cell.status = .fail
+                nextCell.status = .fail
+            }
+        } else {
+            cell.status = editing ? .none : .fail
+        }
+    }
+    
+    private func verifyPasswords(cell: EditableTextCell, cell2: EditableTextCell, editing: Bool = false) {
+        let pass = cell.textField.text ?? ""
+        let pass2 = cell2.textField.text ?? ""
+        
+        if (pass != pass2 || pass.count < 8) {
+            if !editing {
+                cell.status = .fail
+                cell2.status = .fail
+            } else if pass != pass2 {
+                cell.status = .none
+                cell2.status = .none
+            } else {
+                cell2.status = .fail
+            }
+            parameters["password"] = nil
+        } else {
+            cell.status = .tick
+            cell2.status = .tick
+            parameters["password"] = pass
+        }
+    }
+    
+    
+    // Datasource for Picker View
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ["Unspecified", "Male", "Female", "Non-binary"][row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let cell = tableView.cellForRow(at: IndexPath(row: 1, section: 1))
+        cell?.detailTextLabel?.text = ["Unspecified", "Male", "Female", "Non-binary"][row]
+        parameters["gender"] = String(row - 1)
+    }
+    
+
+
+}
