f0602c9d116f376c5a3fc833e0d28d0b90f99131
Yong Song <songyong2000623@163.com>
2019-08-07 23:47:31 -0700

    Infinite Scrolls Bug Fit

--- Eventure/Events Page/EventsViewController.swift
+++ Eventure/Events Page/EventsViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class EventsViewController: UITableViewController {
     var events = [Event]()
     var cells = [EventCell]()
+    var fetchingmore = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,8 +34,8 @@ class EventsViewController: UITableViewController {
         for _ in 1...20 {
             let e = Event(id: String(Int.random(in: 1...1000)), title: randString(length: 10), time: String(Int.random(in: 1999...2019))+"-"+String(Int.random(in: 1...12))+"-"+String(Int.random(in: 1...31)), location: randString(length: 10), tags: [randString(length: 4),randString(length: 4)], hostTitle: randString(length: 15))
             result.append(e)
-        events.append(contentsOf: result)
         }
+        events.append(contentsOf: result)
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -51,6 +53,7 @@ class EventsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         //return [1, 4, 3, 1][section]
+        
         return events.count
     }
     
@@ -61,15 +64,20 @@ class EventsViewController: UITableViewController {
     }
     
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(events.count)
         let down = scrollView.contentOffset.y
         let contentHeight = scrollView.contentSize.height
         let frameHeight = scrollView.frame.height
         print("\(down)|\(contentHeight)|\(frameHeight)")
-        if (down >= contentHeight - frameHeight) {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-                self.getEvents()
-                self.tableView.reloadData()
-            })
+        if (down > contentHeight - frameHeight) {
+            if (!fetchingmore) {
+                fetchingmore = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    self.getEvents()
+                    self.fetchingmore = false
+                    self.tableView.reloadData()
+                })
+            }
         }
     }
     
