39ae2ea39955e1b24c8a64e7bbf83dc6857ee413
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-08-25 08:42:33 +0800

    Added bottom cell

---
+++ Eventure/Organization Account/DatePickerBottomCell.swift
@@ -0,0 +1,24 @@
+//
+//  DatePickerBottomCell.swift
+//  Eventure
+//
+//  Created by Jia Rui Shan on 2019/8/25.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerBottomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
