796faa7046f7788fe760e2490ad44ac72a345129
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-08-29 00:04:47 -0700

    Updated account view

--- Eventure/Account Page/AccountViewController.swift
+++ Eventure/Account Page/AccountViewController.swift
@@ -9,7 +9,7 @@
 import SwiftyJSON
 import UIKit
 
-class AccountViewController: UIViewController,UITableViewDelegate, UITableViewDataSource  {
+class AccountViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
     private var myArray: NSArray = ["First","Second","Sign In"] //experimental
     
@@ -26,8 +26,9 @@ class AccountViewController: UIViewController,UITableViewDelegate, UITableViewDa
         title = "Me"
 
         //myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
-        myTableView = UITableView()
+        myTableView = UITableView(frame: .zero, style: .grouped)
         myTableView.register(AccountCell.classForCoder(), forCellReuseIdentifier: "MyCell")
+        myTableView.register(SettingsItemCell.classForCoder(), forCellReuseIdentifier: "item")
         myTableView.dataSource = self
         myTableView.delegate = self
         myTableView.backgroundColor = UIColor.init(white: 0.95, alpha: 1)
@@ -57,26 +58,10 @@ class AccountViewController: UIViewController,UITableViewDelegate, UITableViewDa
     //when table view is tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        if indexPath.section == 3 && indexPath.row == 0 { //if the log off/sign in button is clicked
-            if (tableView.cellForRow(at: indexPath)! as! AccountCell).function!.text! == "Sign In" {
-                let login = LoginViewController()
-                let nvc = InteractivePopNavigationController(rootViewController: login)
-                nvc.isNavigationBarHidden = true
-                login.navBar = nvc
-                present(nvc, animated: true, completion: nil)
-            } else {
-                //pop out an alert window
-                let alert = UIAlertController(title: "Do you want to log out?", message: "Changes you've made have been saved.", preferredStyle: .actionSheet)
-                alert.addAction(UIAlertAction(title: "Log out", style: .destructive, handler: { action in
-                    UserDefaults.standard.removeObject(forKey: KEY_ACCOUNT_TYPE)
-                    User.current = nil
-                    MainTabBarController.current.openScreen()
-                }))
-                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-                self.present(alert, animated: true)
-                
-            }
-        } else if indexPath.section == 1 && indexPath.row == 0 { //if the user tries to change account information
+        
+        switch (indexPath.section, indexPath.row) {
+            
+        case (1, 0): // if the user tries to change account information
             if User.current == nil {
                 popLoginReminder()
             } else {
@@ -84,7 +69,9 @@ class AccountViewController: UIViewController,UITableViewDelegate, UITableViewDa
                 personalInfo.hidesBottomBarWhenPushed = true
                 navigationController?.pushViewController(personalInfo, animated: true)
             }
-        } else if indexPath.section == 2 && indexPath.row == 3 { //if user wants to change the tags
+            
+        case (2, 3):
+            //if user wants to change the tags
             if User.current == nil {
                 popLoginReminder()
             } else {
@@ -171,6 +158,27 @@ class AccountViewController: UIViewController,UITableViewDelegate, UITableViewDa
                 }
                 
             }
+            
+        case (4, 0): // if the log out/sign in button is clicked
+            if (User.current == nil) {
+                let login = LoginViewController()
+                let nvc = InteractivePopNavigationController(rootViewController: login)
+                nvc.isNavigationBarHidden = true
+                login.navBar = nvc
+                present(nvc, animated: true, completion: nil)
+            } else {
+                //pop out an alert window
+                let alert = UIAlertController(title: "Do you want to log out?", message: "Changes you've made have been saved.", preferredStyle: .actionSheet)
+                alert.addAction(UIAlertAction(title: "Log out", style: .destructive, handler: { action in
+                    UserDefaults.standard.removeObject(forKey: KEY_ACCOUNT_TYPE)
+                    User.current = nil
+                    MainTabBarController.current.openScreen()
+                }))
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                self.present(alert, animated: true)
+            }
+        default:
+            break
         }
     }
     
@@ -178,85 +186,93 @@ class AccountViewController: UIViewController,UITableViewDelegate, UITableViewDa
     
     //create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") as! AccountCell
-        cell.setup(sectionNum: indexPath.section,rowNum: indexPath.row, type:"Account")
-        if indexPath.section == 0 && indexPath.row == 0 {
-            profilePicture = cell.functionImage.image
-            cell.functionImage.isUserInteractionEnabled = true
-            cell.functionImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:))))
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "item") as! SettingsItemCell
+        
+        switch (indexPath.section, indexPath.row) {
+            
+        case (0, 0):
+            cell.icon.image = User.current?.profilePicture
+            if cell.icon.image == nil && User.current != nil {
+                cell.icon.image = User.current!.gender == .male ? #imageLiteral(resourceName: "default male") : #imageLiteral(resourceName: "default_female")
+            } else {
+                cell.icon.image = #imageLiteral(resourceName: "guest_profile")
+            }
+            cell.imageWidthConstraint.constant = 65
+            cell.heightConstraint.constant = 100
+            cell.spacingConstraint.constant = 18
+            cell.titleLabel.text = "Profile Picture"
+        case (1, 0):
+            cell.icon.image = #imageLiteral(resourceName: "default_user")
+            cell.titleLabel.text = "Manage Account"
+        case (2, 0):
+            cell.icon.image = #imageLiteral(resourceName: "scan").withRenderingMode(.alwaysTemplate)
+            cell.titleLabel.text = "Scan Event Code"
+        case (3, 0):
+            cell.icon.image = #imageLiteral(resourceName: "heart").withRenderingMode(.alwaysTemplate)
+            cell.titleLabel.text = "Favorite Events"
+        case (3, 1):
+            cell.icon.image = #imageLiteral(resourceName: "done")
+            cell.titleLabel.text = "Going"
+        case (3, 2):
+            cell.icon.image = #imageLiteral(resourceName: "subscribe")
+            cell.titleLabel.text = "Subscriptions"
+        case (3, 3):
+            cell.icon.image = #imageLiteral(resourceName: "tag")
+            cell.titleLabel.text = "My Tags"
+        case (4, 0):
+            let cell = UITableViewCell()
+            cell.heightAnchor.constraint(equalToConstant: 50).isActive = true
+            
+            let label = UILabel()
+            label.text = User.current != nil ? "Log Out" : "Log In"
+            label.font = .systemFont(ofSize: 17)
+            label.textAlignment = .center
+            label.translatesAutoresizingMaskIntoConstraints = false
+            cell.addSubview(label)
+            
+            label.centerXAnchor.constraint(equalTo: cell.centerXAnchor).isActive = true
+            label.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
+            
+            return cell
+        default:
+            break
         }
+        
         return cell
-    }
-    
-    //make the size of first cell (profile picture) larger
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 && indexPath.row == 0  {
-            return 110.0
-        }
-        return 55.0
     }
     
     //number of sections
     func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 4
+        return 5
     }
     
     //section titles
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if (section == 1) {
-            return "Personal Information"
-        }
-        if (section == 2) {
-            return "Personal Interests"
-        }
-        if (section == 3) {
-            return "Account"
-        }
-        return ""
+        return [
+            nil,
+            "Personal Information",
+            "Event Check-in",
+            "Personal Interests",
+            nil
+        ][section]
     }
     
     //rows in sections
     func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
-        if(section == 0) {
-            return 1
-        } else if(section == 1) {
-            return 1
-        } else if (section == 2){
-            return 4
-        } else {
-            return 1
-        }
+        return [1, 1, 1, 4, 1][section]
     }
     
     //eliminate first section header
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return section == 0 ? 1.0 : 32
+        return [0, 30, 30, 30, 30][section]
     }
     
     
     //full screen profile picture when tapped
     @objc private func imageTapped(_ sender: UITapGestureRecognizer) {
-        let sv = UIScrollView(frame: UIScreen.main.bounds)
-        sv.backgroundColor = .white
-        sv.maximumZoomScale = 3.0
-        sv.minimumZoomScale = 1.0
-        sv.delegate = self
-        
-        let iv = UIImageView(image: profilePicture)
-        iv.contentMode = .center
-        currentImageView = iv
-        iv.frame = sv.frame
-        
-        sv.addSubview(iv)
-        
-        
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
-        sv.addGestureRecognizer(tap)
-        
-        self.view.addSubview(sv)
-        self.navigationController?.isNavigationBarHidden = true
+        let fullScreen = ImageFullScreenPage(image: profilePicture)
+        present(fullScreen, animated: true, completion: nil)
     }
     
     //exit profile picture fullscreen
